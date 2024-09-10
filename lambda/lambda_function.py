eb4eb74b66c4f5a5ab0a0955a21bda6014fa4d95
Ankit Dixit <ankitdixit094@gmail.com>
2024-09-11 02:43:28 +0530

    Update AWS lambda function

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -6,12 +6,10 @@ import cv2
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.utils import CustomObjectScope
-# from ultralytics import YOLO
 
 S3_BUCKET_NAME = os.environ["BUCKET_NAME"]
 UNET0_MODEL_FILE = os.environ["UNET0_MODEL_FILE"]
 UNET1_MODEL_FILE = os.environ["UNET1_MODEL_FILE"]
-YOLO_MODEL_FILE = os.environ["YOLO_MODEL_FILE"]
 s3 = boto3.client("s3")
 
 
@@ -76,7 +74,7 @@ def save_results(image, y_pred, save_image_path):
     put_img_to_s3_object(S3_BUCKET_NAME, save_image_path, image_bytes)
 
 
-def save_results_all(image, y_pred_unet0, y_pred_unet1, y_pred_yolo, save_image_path):
+def save_results_all(image, y_pred_unet0, y_pred_unet1, save_image_path):
     ## i - m - y
     line = np.ones((H, 10, 3)) * 128
 
@@ -90,21 +88,15 @@ def save_results_all(image, y_pred_unet0, y_pred_unet1, y_pred_yolo, save_image_
     y_pred_unet1 = np.concatenate([y_pred_unet1, y_pred_unet1, y_pred_unet1], axis=-1)  ## (512, 512, 3)
     y_pred_unet1 = y_pred_unet1 * 255
 
-    """ Predicted Mask """
-    y_pred_yolo = np.expand_dims(y_pred_yolo, axis=-1)  ## (512, 512, 1)
-    y_pred_yolo = np.concatenate([y_pred_yolo, y_pred_yolo, y_pred_yolo], axis=-1)  ## (512, 512, 3)
-    y_pred_yolo = y_pred_yolo * 255
-
-    cat_images = np.concatenate([image, line, y_pred_unet0, line, y_pred_unet1, line, y_pred_yolo], axis=1)
+    cat_images = np.concatenate([image, line, y_pred_unet0, line, y_pred_unet1], axis=1)
     image_bytes = cv2.imencode(".jpg", cat_images)[1].tobytes()
     put_img_to_s3_object(S3_BUCKET_NAME, save_image_path, image_bytes)
-
 
 
 def validate_input(event):
     s3_input_file = event.get("s3_input_file", None)
     model_type = event.get("model_type", None)
-    model_type_choices = ["unet0", "unet1", "yolo", "all"]
+    model_type_choices = ["unet0", "unet1", "all"]
     if s3_input_file and model_type:
         if model_type not in model_type_choices:
             status = {
@@ -119,6 +111,26 @@ def validate_input(event):
     return status, s3_input_file, model_type
 
 
+def create_dir(path):
+    """Create a directory."""
+    try:
+        if not os.path.exists(path):
+            os.makedirs(path)
+    except FileExistsError as e:
+        pass
+
+
+def save_or_get_model(model_name, model_path):
+    create_dir("/tmp/save_model")
+    local_model_file = f"/tmp/save_model/model-{model_name}.keras"
+    if not os.path.isfile(local_model_file):
+        object_buffer = get_s3_object_to_buffer(S3_BUCKET_NAME, model_path)
+        temp_file = tempfile.NamedTemporaryFile(delete=False, dir="/tmp/save_model", prefix="model-", suffix=".keras")
+        temp_file.write(object_buffer)
+        os.rename(temp_file.name, local_model_file)
+    return local_model_file
+
+
 def get_unet0_output(s3_input_file, save=True):
     result_file = s3_input_file.split("/")[-1]
     result_file = f"lambda_api_predict/unet0/{result_file}"
@@ -130,15 +142,16 @@ def get_unet0_output(s3_input_file, save=True):
     x0 = np.expand_dims(x0, axis=0)
 
 
-    object_buffer = get_s3_object_to_buffer(S3_BUCKET_NAME, UNET0_MODEL_FILE)
-    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".keras")
-    temp_file.write(object_buffer)
-    
+    # object_buffer = get_s3_object_to_buffer(S3_BUCKET_NAME, UNET0_MODEL_FILE)
+    # temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".keras")
+    # temp_file.write(object_buffer)
+    local_model_file = save_or_get_model("unet0", UNET0_MODEL_FILE)
+
     y_pred = None
     with CustomObjectScope(
         {"iou": iou, "dice_coef": dice_coef, "dice_loss": dice_loss}
     ):
-        model = tf.keras.models.load_model(temp_file.name, safe_mode=False)
+        model = tf.keras.models.load_model(local_model_file, safe_mode=False)
 
         """ Prediction """
         y_pred = model.predict(x0)[0]
@@ -166,15 +179,16 @@ def get_unet1_output(s3_input_file, save=True):
     x0 = np.expand_dims(x0, axis=0)
 
 
-    object_buffer = get_s3_object_to_buffer(S3_BUCKET_NAME, UNET1_MODEL_FILE)
-    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".keras")
-    temp_file.write(object_buffer)
+    # object_buffer = get_s3_object_to_buffer(S3_BUCKET_NAME, UNET1_MODEL_FILE)
+    # temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".keras")
+    # temp_file.write(object_buffer)
+    local_model_file = save_or_get_model("unet1", UNET1_MODEL_FILE)
     
     y_pred = None
     with CustomObjectScope(
         {"iou": iou, "dice_coef": dice_coef, "dice_loss": dice_loss}
     ):
-        model = tf.keras.models.load_model(temp_file.name, safe_mode=False)
+        model = tf.keras.models.load_model(local_model_file, safe_mode=False)
         
         """ Prediction """
         y_pred = model.predict(x0)[0]
@@ -191,40 +205,6 @@ def get_unet1_output(s3_input_file, save=True):
         return y_pred
 
 
-def get_yolo_output(s3_input_file, save=True):
-    # result_file_name = s3_input_file.split("/")[-1]
-    # result_file = f"lambda_api_predict/yolo/{result_file_name}"
-    # x0 = np.frombuffer(get_s3_object_to_buffer(S3_BUCKET_NAME, s3_input_file), np.uint8)
-
-    # """ Reading the image """
-    # image = cv2.imdecode(x0, cv2.IMREAD_COLOR + cv2.IMREAD_IGNORE_ORIENTATION)
-    # x0 = image / 255.0
-    # x0 = np.expand_dims(x0, axis=0)
-
-
-    # object_buffer = get_s3_object_to_buffer(S3_BUCKET_NAME, YOLO_MODEL_FILE)
-    # temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".keras")
-    # temp_file.write(object_buffer)
-    
-    # model = YOLO(temp_file.name)
-    # y_pred = model.predict(x0, conf=0.1, visualize=False, save=False)
-    
-    # y_pred[0].save(result_file_name)
-    # y_pred = cv2.imread(result_file_name, cv2.IMREAD_COLOR + cv2.IMREAD_IGNORE_ORIENTATION)
-    
-    # if save:
-    #     """ Saving the prediction """
-    #     save_results(image, y_pred, result_file)
-    #     result_image_url = get_img_public_url(S3_BUCKET_NAME, result_file)
-    #     return result_image_url
-    # else:
-    #     return y_pred
-
-    return get_unet1_output(s3_input_file, save)
-        
-
-
-
 def get_all_output(s3_input_file, save=True):
     result_file_name = s3_input_file.split("/")[-1]
     result_file = f"lambda_api_predict/all/{result_file_name}"
@@ -235,13 +215,9 @@ def get_all_output(s3_input_file, save=True):
 
     y_pred_unet0 = get_unet0_output(s3_input_file, save=False)
     y_pred_unet1 = get_unet1_output(s3_input_file, save=False)
-    y_pred_yolo = get_yolo_output(s3_input_file, save=False)
-    save_results_all(image, y_pred_unet0, y_pred_unet1, y_pred_yolo, result_file)
+    save_results_all(image, y_pred_unet0, y_pred_unet1, result_file)
     result_image_url = get_img_public_url(S3_BUCKET_NAME, result_file)
     return result_image_url
-
-
-
 
 
 def predict_output(s3_input_file, model_type):
@@ -249,8 +225,6 @@ def predict_output(s3_input_file, model_type):
         result_image_url = get_unet0_output(s3_input_file)
     elif model_type == "unet1":
         result_image_url = get_unet1_output(s3_input_file)
-    elif model_type == "yolo":
-        result_image_url = get_yolo_output(s3_input_file)
     elif model_type == "all":
         result_image_url = get_all_output(s3_input_file)
     return result_image_url
