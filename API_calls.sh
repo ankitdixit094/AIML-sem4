9adaa83b3edb32a9e9ca4ac84443754634bf1fa3
Ankit Dixit <ankitdixit094@gmail.com>
2024-09-13 21:22:09 +0530

    Update API Calls curl

---
+++ API_calls.sh
@@ -0,0 +1,17 @@
+curl -X POST -H "Content-Type: application/json" -d '{"s3_input_file": "AIML-sem4/ad_unet_code0/data/val_data/512x512/images/TF26_15_33_20170714_172342.jpg", "model_type": "unet0"}' https://vpa4fsajy8.execute-api.us-east-1.amazonaws.com/dev/
+curl -X POST -H "Content-Type: application/json" -d '{"s3_input_file": "AIML-sem4/ad_unet_code0/data/val_data/512x512/images/TF26_15_33_20170714_172342.jpg", "model_type": "unet1"}' https://vpa4fsajy8.execute-api.us-east-1.amazonaws.com/dev/
+
+
+curl -X POST -H "Content-Type: application/json" -d '{"s3_input_file": "AIML-sem4/ad_unet_code0/data/val_data/512x512/images/TF29_15_1_36_20170724_141719.jpg", "model_type": "unet0"}' https://vpa4fsajy8.execute-api.us-east-1.amazonaws.com/dev/
+curl -X POST -H "Content-Type: application/json" -d '{"s3_input_file": "AIML-sem4/ad_unet_code0/data/val_data/512x512/images/TF29_15_1_36_20170724_141719.jpg", "model_type": "unet1"}' https://vpa4fsajy8.execute-api.us-east-1.amazonaws.com/dev/
+
+
+curl -X POST -H "Content-Type: application/json" -d '{"s3_input_file": "AIML-sem4/ad_unet_code0/data/val_data/512x512/images/TF23_C108P69_20170709_122110.jpg", "model_type": "unet0"}' https://vpa4fsajy8.execute-api.us-east-1.amazonaws.com/dev/
+curl -X POST -H "Content-Type: application/json" -d '{"s3_input_file": "AIML-sem4/ad_unet_code0/data/val_data/512x512/images/TF23_C108P69_20170709_122110.jpg", "model_type": "unet1"}' https://vpa4fsajy8.execute-api.us-east-1.amazonaws.com/dev/
+
+
+
+
+
+
+
